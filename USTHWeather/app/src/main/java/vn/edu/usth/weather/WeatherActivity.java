abd6b55fc46be3802750bc605b3bda9941c73171
DarkHent <khoinvh.22bi1322@usth.edu.vn>
2024-08-30 13:27:27 +0700

    Practical Work 2

---
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -0,0 +1,57 @@
+package vn.edu.usth.weather;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class WeatherActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+        Log.i("WeatherActivity", "App is created");
+        ForecastFragment firstFragment = new ForecastFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.container, firstFragment).commit();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("WeatherActivity", "App is started");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("WeatherActivity", "App is resumed");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("WeatherActivity", "App is paused");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("WeatherActivity", "App is stopped");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("WeatherActivity", "App is destroyed");
+    }
+
+
+}
